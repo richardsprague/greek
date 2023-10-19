73f1a7be59940b8079f0cedec8536a95476bcd1d
Richard Sprague <richardsprague@gmail.com>
2023-10-18 19:43:35 -0700

    full greek alphabet, show just one card and click next for next card

--- koine-greek-flashcards/pages/index.tsx
+++ koine-greek-flashcards/pages/index.tsx
@@ -1,13 +1,40 @@
-import Head from 'next/head';
-import FlashcardContainer from '../components/FlashcardContainer';
+import { useState, useEffect } from 'react';
+import greekAlphabet from '../data/greekAlphabet.json';
 
-export default function Home() {
+const App = () => {
+  const [currentCard, setCurrentCard] = useState(null);
+  const [flipped, setFlipped] = useState(false);
+
+  useEffect(() => {
+    getRandomCard();
+  }, []);
+
+  const getRandomCard = () => {
+    setFlipped(false);  // Reset the flip state when a new card is generated
+
+    const randomIndex = Math.floor(Math.random() * greekAlphabet.length);
+    const randomCard = greekAlphabet[randomIndex];
+    setCurrentCard(randomCard);
+  };
+
+  const toggleFlip = () => {
+    setFlipped(!flipped);
+  };
+
   return (
-    <div>
-      <Head>
-        <title>Koine Greek Flashcards</title>
-      </Head>
-      <FlashcardContainer />
+    <div className="container flex flex-col h-screen bg-gray-100 justify-center items-center">
+      <div className="flashcard-container flex flex-wrap justify-center items-center">
+        {currentCard && (
+          <div onClick={toggleFlip} className="flashcard rounded-lg border-4 border-black bg-beige text-blue-500 flex items-center justify-center h-32 w-32 m-4">
+            <div className={flipped ? 'hidden' : 'block'}>{currentCard.greek}</div>
+            <div className={flipped ? 'block' : 'hidden'}>{currentCard.english}</div>
+          </div>
+        )}
+      </div>
+      <button type="button" onClick={getRandomCard} className="bg-blue-500 text-white p-2 rounded">Next</button>
     </div>
   );
-}
+  
+};
+
+export default App;
